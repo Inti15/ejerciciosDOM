09d90af30211e5c39b8f6675c75a74cc924da481
Yesenia <yeseniasescarcega@gmail.com>
2017-03-22 11:03:39 -0600

    Se agregó diseño a los archivos del ejercicio 3 de DOM

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -6,6 +6,7 @@ var email = document.getElementById("email").value;//Se obtiene el valor del ele
 document.getElementById("name").value = "";// Se limpia el contenido del elemento con el id = name
 document.getElementById("email").value = "";// Se limpia el contenido del elemento con el id = email
 
+document.getElementById("datos").innerHTML = "Datos ingresados:"//Se asigna un texto a la etiqueta <h3> que coincide con el id
 document.getElementById("mostrarNombre").innerHTML = nombre; // Se escribe en el elemento con el id = mostrarNombre el valor de la variable nombre
 document.getElementById("mostrarMail").innerHTML = email; // Se escribe en el elemento con el id = mostrarMail el valor de la variable email
 }
